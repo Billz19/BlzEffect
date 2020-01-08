8d8c8cea7cd3e64b70b1cfa9c3019d295de6a641
Bill Lam <mildelbil10@hotmail.fr>
2020-01-08 21:10:47 +0100

    add promises to slide effects

--- app.js
+++ app.js
@@ -5,20 +5,23 @@ export default class Effects {
      * @param {Number} duration
      */
     static slideUp(element, duration = 500) {
-        let propsArray = ['height', 'padding-top', 'padding-bottom', 'margin-top', 'margin-bottom', 'border-width'];
-        element.style.height = element.offsetHeight+ 'px';
-        element.style.transitionProperty = 'height, padding, margin';
-        element.style.transitionDuration = duration + 'ms';
-        element.offsetHeight; //redraw
-        element.style.overflow = 'hidden';
-        propsArray.forEach(prop =>
-            element.style[prop] = 0
-        );
-        window.setTimeout(function () {
-            element.style.display = 'none';
-            propsArray = ['transition-property','transition-duration','overflow',...propsArray];
-            propsArray.forEach(prop => element.style.removeProperty(prop))
-        },duration)
+        return new Promise(resolve => {
+            let propsArray = ['height', 'padding-top', 'padding-bottom', 'margin-top', 'margin-bottom', 'border-width'];
+            element.style.height = element.offsetHeight + 'px';
+            element.style.transitionProperty = 'height, padding, margin';
+            element.style.transitionDuration = duration + 'ms';
+            element.offsetHeight; //redraw
+            element.style.overflow = 'hidden';
+            propsArray.forEach(prop =>
+                element.style[prop] = 0
+            );
+            window.setTimeout(function () {
+                element.style.display = 'none';
+                propsArray = ['transition-property', 'transition-duration', 'overflow', ...propsArray];
+                propsArray.forEach(prop => element.style.removeProperty(prop));
+                resolve(element);
+            }, duration)
+        });
     }
 
     /**
@@ -27,23 +30,26 @@ export default class Effects {
      * @param {Number} duration
      */
     static slideDown(element, duration = 500) {
-       element.style.removeProperty('display');
-       let display = window.getComputedStyle(element).display;
-       if (display === 'none') display = 'block';
-       element.style.display = display;
-       let height = element.offsetHeight,
-           propsArray = ['height','padding-top','padding-bottom','margin-top','margin-bottom','border-width'];
-       element.style.overflow = 'hidden';
-       propsArray.forEach(prop => element.style[prop] = 0);
-       element.offsetHeight; // redraw
-       element.style.transitionProperty = 'height, margin, padding';
-       element.style.transitionDuration = duration + 'ms';
-       element.style.height = height + 'px';
-       propsArray.slice(1).forEach(prop => element.style.removeProperty(prop));
-       propsArray = ['transition-property','transition-duration','overflow',...propsArray];
-       window.setTimeout(function () {
-          propsArray.slice(0,4).forEach(prop => element.style.removeProperty(prop));
-       },duration)
+        return new Promise(resolve => {
+            element.style.removeProperty('display');
+            let display = window.getComputedStyle(element).display;
+            if (display === 'none') display = 'block';
+            element.style.display = display;
+            let height = element.offsetHeight,
+                propsArray = ['height', 'padding-top', 'padding-bottom', 'margin-top', 'margin-bottom', 'border-width'];
+            element.style.overflow = 'hidden';
+            propsArray.forEach(prop => element.style[prop] = 0);
+            element.offsetHeight; // redraw
+            element.style.transitionProperty = 'height, margin, padding';
+            element.style.transitionDuration = duration + 'ms';
+            element.style.height = height + 'px';
+            propsArray.slice(1).forEach(prop => element.style.removeProperty(prop));
+            propsArray = ['transition-property', 'transition-duration', 'overflow', ...propsArray];
+            window.setTimeout(function () {
+                propsArray.slice(0, 4).forEach(prop => element.style.removeProperty(prop));
+                resolve(element);
+            }, duration)
+        });
     }
 
     /**
@@ -52,11 +58,13 @@ export default class Effects {
      * @param {Number} duration
      */
     static slideToggle(element, duration = 500) {
-       const display = window.getComputedStyle(element).display;
-       if (display === 'none')
-           this.slideDown(element,duration);
-       else
-           this.slideUp(element,duration);
+        return new Promise(resolve => {
+            const display = window.getComputedStyle(element).display;
+            if (display === 'none')
+                this.slideDown(element, duration).then(elem => resolve(elem));
+            else
+                this.slideUp(element, duration).then(elem => resolve(elem));
+        });
     }
 }
 
